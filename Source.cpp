320dda19c0e3198b6c09dfc217ecdd28c03475c5
Jahnava <jahnavalmt@gmail.com>
2020-09-18 11:32:35 -0400

    added include directives

--- Source.cpp
+++ Source.cpp
@@ -1,3 +1,11 @@
-//hgfiygvljfbalfhvlefgv
-//uygfiagfaiufg;ia
-//lusygflihsfbvlhdfb
+//converts seconds into days, hours, or minutes
+
+//include directives
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+
+}
